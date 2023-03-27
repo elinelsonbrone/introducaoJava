58c2e40aa6621fbd901c3205478ed69f1e8fde8d
elinelsonbrone <elinelsonbrone@gmail.com>
2023-03-27 19:12:22 -0300

    Atualizando Material de Construcao

---
+++ 1 Ano/Java POO/MaterialDeContrucao/src/modelo/Cupom.java
@@ -0,0 +1,35 @@
+package modelo;
+
+public class Cupom {
+	private int quantidade;
+	private double valorTotal;
+	private String descricao;
+	private String data;
+	
+	public String getData() {
+		return data;
+	}
+	public void setData(String data) {
+		this.data = data;
+	}
+	public int getQuantidade() {
+		return quantidade;
+	}
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+	public double getValorTotal() {
+		return valorTotal;
+	}
+	public void setValorTotal(double valorTotal) {
+		this.valorTotal = valorTotal;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+
+}
